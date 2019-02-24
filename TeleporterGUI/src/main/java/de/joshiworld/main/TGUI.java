e28a02321aac46c5a3591bd15a1e29c6a2cf3c00
JoshiWorld <joshua.stieber.1903@gmail.com>
2019-02-24 23:33:22 +0100

    Update TGUI.java

--- TeleporterGUI/src/main/java/de/joshiworld/main/TGUI.java
+++ TeleporterGUI/src/main/java/de/joshiworld/main/TGUI.java
@@ -14,17 +14,17 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class TGUI extends JavaPlugin {
     
-    public static TGUI instance;
+    public static TGUI instance; //Getter der Main-Class
     
     @Override
     public void onEnable() {
         
-        PluginManager pm = Bukkit.getServer().getPluginManager();
+        PluginManager pm = Bukkit.getServer().getPluginManager(); //Der PluginManager ist da, um Listener in der Main zu registrieren
         pm.registerEvents(new PlayerJoinListener(), this);
         pm.registerEvents(new InventoryClickListener(), this);
         pm.registerEvents(new PlayerInteractListener(), this);
         
-        this.getCommand("tgui").setExecutor(new TGUI_Command());
+        this.getCommand("tgui").setExecutor(new TGUI_Command()); //Damit registrierst du ein Command
     }
     
     @Override
